05069e018ee7e920065d999f3b7926a0e7d48803
BrianDYKim <18k7102dy@gmail.com>
2024-11-24 15:36:33 +0900

    project init

---
+++ src/main/kotlin/team/brian/sprintstudy/SprintStudyApplication.kt
@@ -0,0 +1,11 @@
+package team.brian.sprintstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SprintStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SprintStudyApplication>(*args)
+}
---
+++ src/test/kotlin/team/brian/sprintstudy/SprintStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.brian.sprintstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SprintStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
