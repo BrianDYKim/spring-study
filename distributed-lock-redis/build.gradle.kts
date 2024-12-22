3414d67ae0540ef117f1f29a01285d3c891ae1d8
BrianDYKim <18k7102dy@gmail.com>
2024-12-22 18:41:34 +0900

    distributed-lock-redis: gradle 의존성 설정 완료

--- distributed-lock-redis/build.gradle.kts
+++ distributed-lock-redis/build.gradle.kts
@@ -1,3 +1,22 @@
-dependencies {
+val queryDslVersion = "5.1.0"
 
+dependencies {
+    // Spring Data Redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+    // JPA
+    runtimeOnly("com.mysql:mysql-connector-j")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // Query DSL
+    implementation("com.querydsl:querydsl-jpa:$queryDslVersion:jakarta")
+    kapt("com.querydsl:querydsl-apt:$queryDslVersion:jakarta")
+    kapt("jakarta.annotation:jakarta.annotation-api")
+    kapt("jakarta.persistence:jakarta.persistence-api")
+}
+
+tasks {
+    named<Delete>("clean") {
+        delete("src/main/generated")
+    }
 }
